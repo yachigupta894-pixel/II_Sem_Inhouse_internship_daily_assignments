86d3f4ec0a91dcc646515f4a8012c8fd9319dd34
yachigupta894-pixel <yachigupta894@gmail.com>
2026-07-12 15:32:32 +0530

    Add success page for registration confirmation

---
+++ 25ESKCA247 /DAY11 /success.php
@@ -0,0 +1,15 @@
+<?php
+include("header.php");
+?>
+
+<div class="container mt-5 text-center">
+    <div class="alert alert-success">
+        <h3>Registration Successful!</h3>
+        <p>your form has been submitted successfully.</p>
+</div>
+
+<a href="login.php" class="btn btn-primary">Go to Back</a>
+</div>
+
+<?php
+include("footer.php");

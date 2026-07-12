f946cb15a3319d4f0a8957cb869195e2d25246f6
yachigupta894-pixel <yachigupta894@gmail.com>
2026-07-12 15:31:03 +0530

    Add header.php with website structure and navigation

---
+++ 25ESKCA247 /DAY11 /header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+
+    <link href="bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <header  class="bg-light border-bottom">
+        <div class="container">
+            <div class="d-flex justify-content-between align-itemm-center py-3">
+
+                <img src="c:\Users\smile\Downloads\logo.png" alt="logo" width="80">
+
+                <nav>
+                    <ul class="nav" style="font-size: 16px; font-weight:bold; gap: 80px;">
+                        <li class="nav-item">
+                            <a href="index.php" class="nav-link text-dark">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="about.php" class="nav-link text-dark">About Us</a>
+                        </li>
+                        
+                        <li class="nav-item">
+                            <a href="contact.php" class="nav-link text-dark">Service</a>
+                        </li> 
+
+                        <li class="nav-item">
+                            <a href="contact.php" class="nav-link text-dark">Contact Us</a>
+                        </li>  
+                    </ul> 
+                </nav> 
+
+                <button type="button" class="btn btn-primary">
+                        login
+                </button>
+
+            </div>
+        </div>
+    </header>

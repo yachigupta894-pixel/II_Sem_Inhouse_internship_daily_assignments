79b4f0e76cab5ec350c2a47c19c4a2e9768d85f0
yachigupta894-pixel <yachigupta894@gmail.com>
2026-07-12 15:30:20 +0530

    Add footer to footer.php
    Add footer section with copyright and Bootstrap script.

---
+++ 25ESKCA247 /DAY11 /footer.php
@@ -0,0 +1,6 @@
+<footer class="bg-dark text-white text-center py-3 mt-5">
+            <p class="mb-0">&copy;<?php echo date("Y");?>My Website.All Rights Reserved.</p>
+        </footer>
+        <script src="bootstrap.bundle.min.js"></script>
+    </body>
+</html>
